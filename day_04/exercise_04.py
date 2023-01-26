3a47dace1ade75e9b422ce6ef9f1e87551059550
Bangis <bsani480@gmail.com>
2023-01-26 21:35:04 +0100

    day 04

---
+++ day_04/exercise_04.py
@@ -0,0 +1,27 @@
+# exercise day 04
+
+# number 1
+str1 = 'Thirty'
+str2 = 'Days'
+str3 = 'Of'
+str4 = 'Python'
+space = ' '
+sentence = str1 + space + str2 + space + str3 + space + str4
+print(sentence)
+
+#number 2
+str_1 = 'Coding'
+str_2 = 'For'
+str_3 = 'All'
+statements = str_1 + space + str_2 + space + str_3
+print(statements)
+
+# number 3 & 4 & 5 &
+company = 'Coding For All'
+print(company)
+print(len(company))
+print(company.upper())
+print(company.lower())
+print(company.title())
+print(company.capitalize())
+print(company.swapcase())
