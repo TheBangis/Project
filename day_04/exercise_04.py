f4145fce99efd24b38714fea5ee7e25a717f3247
Bangis <bsani480@gmail.com>
2023-01-27 21:02:28 +0100

    day 04

--- day_04/exercise_04.py
+++ day_04/exercise_04.py
@@ -36,8 +36,58 @@ print('Coding For All'.split(' '))
 tech_firm = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon"
 print(tech_firm.split(','))
 
-#number 
+#number 15-17
 challenge = 'Coding For All'
 print(challenge[0])
 print(challenge[-1])
 print(challenge[10])
+
+#18-19
+
+print(challenge.index('C'))
+print(challenge.index('F'))
+
+print('Coding For All People'.rfind('l'))
+
+s_challenge = 'You cannot end a sentence with because because because is a conjunction'
+print(s_challenge.find('because'))
+print(s_challenge.rfind('because'))
+print(s_challenge[31:54])
+print(challenge.startswith('Coding'))
+print(challenge.startswith('coding'))
+
+r_char = '   Coding For All      '
+print(r_char.strip('   '))
+
+# Number 31
+check_1, check_2 = '30DaysOfPython', 'thirty_days_of_python'
+print(check_1.isidentifier())
+print(check_2.isidentifier())
+
+# Number 32
+lists = 'Django', 'Flask', 'Bottle', 'Pyramid', 'Falcon'
+result = '# '.join(lists)
+print(result)
+
+# Number 33
+print('I am enjoying this challenge.\nI just wonder what is next.')
+
+# Number 34
+print('Name\tAge\tCountry\tCity')
+print('Asabeneh\t250\tFinland\tHelsinki')
+
+# Number 35 
+radius = 10
+area = 3.14 * radius ** 2
+print('The area of a circle with radius 10 is {} meters square.'.format(area))
+
+# Number 36
+a = 8
+b = 6
+print('{} + {} = {}'.format(a, b, a + b))
+print('{} - {} = {}'.format(a, b, a - b))
+print('{} * {} = {}'.format(a, b, a * b))
+print('{} / {} = {}'.format(a, b, a / b))
+print('{} % {} = {}'.format(a, b, a % b))
+print('{} // {} = {}'.format(a, b, a // b))
+print('{} ** {} = {}'.format(a, b, a ** b))
