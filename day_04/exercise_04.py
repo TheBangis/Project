43a9f6518531d56e897922ba325e02ee61b9e2a5
Bangis <bsani480@gmail.com>
2023-01-27 19:40:16 +0100

    day 04

--- day_04/exercise_04.py
+++ day_04/exercise_04.py
@@ -16,7 +16,7 @@ str_3 = 'All'
 statements = str_1 + space + str_2 + space + str_3
 print(statements)
 
-# number 3 & 4 & 5 &
+# number 3 - 9
 company = 'Coding For All'
 print(company)
 print(len(company))
@@ -25,3 +25,19 @@ print(company.lower())
 print(company.title())
 print(company.capitalize())
 print(company.swapcase())
+print(company[0:6])
+statement_index = 'Coding Coding For All'
+check_index = 'Coding'
+print(statement_index.index(check_index)) #or print('Coding' in 'Coding Coding For All')
+f_string = 'coding for all'
+print(f_string.replace('coding', 'python')) # or print('coding for all'.replace('coding', 'python'))
+print('Python for Everyone'.replace('Everyone', 'All'))
+print('Coding For All'.split(' '))
+tech_firm = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon"
+print(tech_firm.split(','))
+
+#number 
+challenge = 'Coding For All'
+print(challenge[0])
+print(challenge[-1])
+print(challenge[10])
