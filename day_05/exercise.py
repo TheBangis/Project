6335be0ed17ca2485221c4f0da61190ab1e2ac92
Bangis <bsani480@gmail.com>
2023-01-28 20:54:35 +0100

    day 05

--- day_05/exercise.py
+++ day_05/exercise.py
@@ -1,5 +1,5 @@
 # Exercise Day 05
-
+""""
 # Number 01
 lst = []
 
@@ -42,3 +42,77 @@ print(last_company)
 it_companies[1] = 'ChatGPT'
 print(it_companies)
 
+# Number 11
+it_companies.append('Ubuntu')
+print(it_companies)
+
+# Number 12
+it_companies.insert(4, 'Shamrock')
+print(it_companies)
+
+# Number 13
+it_companies[0] = 'FACEBOOK'
+print(it_companies)
+
+# Number 14
+
+# Number 15
+does_exist = 'Facebook' in it_companies
+print(does_exist)
+
+# Number 16
+it_companies.sort()
+print(it_companies)
+
+# Number 17
+it_companies.reverse()
+print(it_companies)
+
+# Number 18
+print(it_companies[0:3])
+
+# Number 19
+print(it_companies[-3::])
+
+# Number 20
+print(it_companies[4:9:5])
+
+# Number 21
+it_companies.pop(0)
+print(it_companies)
+
+# Number 22
+del it_companies[3]
+print(it_companies)
+
+# Number 23
+it_companies.pop()
+print(it_companies)
+
+# Number 24
+it_companies.clear()
+print(it_companies)
+
+# Number 25
+del it_companies
+
+# Number 26
+front_end = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+back_end = ['Node','Express', 'MongoDB']
+front_end.extend(back_end)
+print(front_end)
+
+# Number 27
+full_stack = front_end.copy()
+full_stack.insert(5, 'Python')
+full_stack.insert(5, 'SQL')
+print(full_stack)"""
+
+""" Challenge: 
+Exercise (2) Day 05"""
+
+# Number 01
+
+ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+ages.sort()
+print(ages)
