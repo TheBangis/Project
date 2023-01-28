fa9bdb290efadc0aa5d380c85898cb5c6e7244c6
Bangis <bsani480@gmail.com>
2023-01-28 17:07:23 +0100

    day 05

---
+++ day_05/exercise.py
@@ -0,0 +1,44 @@
+# Exercise Day 05
+
+# Number 01
+lst = []
+
+# Number 02
+phones = ['infinix', 'Techno', 'Ridimi', 'iphone', 'nokia', 'google pixel']
+
+# Number 03
+length = len(phones)
+print(length)
+
+# Number 04
+first_item = phones[0]
+print(first_item)
+middle_item = phones[3]
+print(middle_item)
+last_item = phones[-1]
+print(last_item)
+
+# Number 05
+mixed_data_types = ['babangida', '25', '1.91', 'single', 'dan rimi']
+
+# Number 06
+it_companies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
+
+# Number 07
+print(it_companies)
+
+# Number 08
+print(len(it_companies))
+
+# Number 09
+first_company = it_companies[0]
+print(first_company)
+second_company = it_companies[3]
+print(second_company)
+last_company = it_companies[-1]
+print(last_company)
+
+# Number 10
+it_companies[1] = 'ChatGPT'
+print(it_companies)
+
