62933cb21832e07ab7d26402ba008d3a24178c2d
Bangis <bsani480@gmail.com>
2023-02-13 09:48:29 +0100

    first commit

--- day_01/euclidian_distance.py
+++
@@ -1,8 +0,0 @@
-import math
-#Exercise 3, No 2.
-#Eculidian Distance between (2, 3) and (10, 8)
-
-x1, x2 = 2, 10
-y1, y2 = 3, 8
-d = math.sqrt(pow((x2 - x1), 2) + pow((y2 - y1), 2))
-print('Euclidian Distance: {}'.format(d))
--- day_12/exercise_2.py
+++
@@ -1,7 +0,0 @@
-''' 30 Days of Python - Day 12
-Exercise 2: level 2 --------------- Modules'''
-
-from random import random, randint
-import string
-import random
-
